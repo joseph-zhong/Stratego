2003c75ec41d3741e25c5135904ea77cd6b91547
Joseph Zhong <glub.bob@gmail.com>
2014-05-24 16:18:33 -0700

    Added InfoGUI

---
+++ BoardGame/src/boardgame/InfoGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package boardgame;
+
+/**
+ *
+ *  Joseph Zhong
+ *  ASSIGNMENT_NUMBER
+ *  PROGRAM_DESCRIPTION
+ *  PROGRAM_TITLE
+ *  DATE
+ *
+ **/
+
+public class InfoGUI 
+{
+
+}
