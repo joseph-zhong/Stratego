975d787b4a622d698a9c8305ca4f1a24dc6f4404
Joseph Zhong <glub.bob@gmail.com>
2014-05-24 15:45:05 -0700

    Updated MainGUI

--- BoardGame/src/boardgame/MainGUI.java
+++ BoardGame/src/boardgame/MainGUI.java
@@ -17,8 +17,14 @@ package boardgame;
 
 public class MainGUI
 {
+    // constants for the drawing panel size, tile sizes, and # of tiles
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 800;
+    public static final int MIN_SIZE = 100;
+    public static final int MAX_SIZE = 200;
+
     public MainGUI()
     {
-        
+        DrawingPanel mainPanel;
     }
 }
