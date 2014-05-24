68173f0af26cef7e7964f945fe3fc6b069202c03
Joseph Zhong <glub.bob@gmail.com>
2014-05-24 15:06:05 -0700

    Changed piece package

--- BoardGame/src/boardgame/Piece.java
+++ BoardGame/src/boardgame/Piece.java
@@ -2,21 +2,21 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Stratego;
+package boardgame;
 
 /**
  *
  * @author Adarsh
  */
 import java.awt.*;
-abstract class Piece 
+abstract class Piece
 {
     private int power;
     private int ability;
     private int distanceCapable;
     private Image face;
     private Cell location;
-    
+
     public Piece(int p, int a, int d, Image f)
     {
         power = p;
@@ -24,21 +24,21 @@ abstract class Piece
         distanceCapable = d;
         face = f;
     }
-    
-    
+
+
     public int getPower()
     { return power; }
-    
+
     public int getAbility()
     { return ability; }
-    
+
     public Cell getCell()
     { return location; }
-    
+
     public int getDistanceCapable()
     { return distanceCapable; }
-    
+
     public Image getFace()
     { return face; }
-    
+
 }
