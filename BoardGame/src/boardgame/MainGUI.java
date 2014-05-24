897d9aef4989dc9691c08c3ba12a34d1dfc94036
Joseph Zhong <glub.bob@gmail.com>
2014-05-24 15:53:17 -0700

    Updated MainGUI

--- BoardGame/src/boardgame/MainGUI.java
+++ BoardGame/src/boardgame/MainGUI.java
@@ -5,6 +5,8 @@
 
 package boardgame;
 
+import java.awt.Graphics;
+
 /**
  *
  *  Joseph Zhong
@@ -23,8 +25,33 @@ public class MainGUI
     public static final int MIN_SIZE = 100;
     public static final int MAX_SIZE = 200;
 
-    public MainGUI()
+    // main Panel for overall display
+    private static DrawingPanel mainPanel;
+
+
+
+    public static void main(String[] arg)
     {
-        DrawingPanel mainPanel;
+
+        DrawingPanel panel = new DrawingPanel(HEIGHT, WIDTH);
+        Graphics g = panel.getGraphics();
+
+        /*
+        // create several random tiles and put them into a manager
+        TileManager list = new TileManager();
+        for (int i = 0; i < TILES; i++) {
+            Tile tile = makeRandomTile();
+            list.addTile(tile);
+        }
+        list.drawAll(g);
+
+        // listen for key presses
+        RectangleKeyListener listener = new RectangleKeyListener(panel, list);
+        panel.addKeyListener(listener);
+
+        // listen for mouse clicks
+        RectangleMouseListener listener2 = new RectangleMouseListener(panel, list);
+        panel.addMouseListener(listener2);
+        * */
     }
 }
