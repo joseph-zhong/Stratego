68abc1da28844b4791046bc9f12f6354d96e3cd7
Joseph Zhong <glub.bob@gmail.com>
2014-05-24 22:26:27 -0700

    Testing v1 CellGUI

--- BoardGame/src/GUI/CellGUI.java
+++ BoardGame/src/GUI/CellGUI.java
@@ -5,6 +5,13 @@
 
 package GUI;
 
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 /**
  *
  *  Joseph Zhong
@@ -15,7 +22,41 @@ package GUI;
  *
  **/
 
-public class CellGUI
+public class CellGUI extends ButtonGUI
 {
+    private BufferedImage PieceImage;
 
+    private boolean isPossibleMove;
+
+    public CellGUI(int _x, int _y, int _width, int _height, Color _c, String _name)
+    {
+        super(_x, _y, _width, _height, _c, _name);
+        isPossibleMove = true;
+    }
+
+    /** Draws this button using the given graphics pen. */
+    @Override
+    public void draw(Graphics g)
+    {
+        super.draw(g);
+        g.setColor(Color.WHITE);
+        g.drawRect(getX(), getY(), getWidth(), getHeight());
+    }
+
+    public void setPiece(BufferedImage _PieceImage)
+    {
+        if(isPossibleMove)
+        {
+            PieceImage = (BufferedImage) _PieceImage.getScaledInstance(getX(), getY(), Image.SCALE_DEFAULT);
+        }
+        isPossibleMove = false;
+    }
+
+    public void removePiece()
+    {
+        if(PieceImage != null)
+        {
+            PieceImage = null;
+        }
+    }
 }
