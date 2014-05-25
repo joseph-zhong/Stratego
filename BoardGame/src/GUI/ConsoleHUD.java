874e56fd4875ef37fdb3f7f40ff88b52596027f9
Joseph Zhong <glub.bob@gmail.com>
2014-05-25 06:14:35 -0700

    Initial HUDS created

---
+++ BoardGame/src/GUI/ConsoleHUD.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package GUI;
+
+/**
+ *
+ *  Joseph Zhong
+ *  ASSIGNMENT_NUMBER
+ *  PROGRAM_DESCRIPTION
+ *  PROGRAM_TITLE
+ *  DATE
+ *
+ **/
+
+public class ConsoleHUD 
+{
+
+}
