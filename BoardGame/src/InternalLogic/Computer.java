a7a6e9c2bfa783dd5661381e13be158b0e9dff72
addu <karnati.adarsh@gmail.com>
2014-05-25 02:25:39 -0700

    New Computer with fixed cell setup

--- BoardGame/src/InternalLogic/Computer.java
+++ BoardGame/src/InternalLogic/Computer.java
@@ -2,21 +2,26 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package InternalLogic;
+package boardgame;
 
 /**
  *
  * @author Adarsh
  */
 import java.util.Random;
-import Pieces.*;
-public class Computer
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Computer 
 {
     public static final int forget = 4; //The number of turns before AI forgets player move
     private Move[] oppMoves = new Move[4];
     private Random r = new Random();
-
-
+    
+    private Board board;
+    
+    private ArrayList<Piece> totalPiecesForSetup;
+    private ArrayList<Piece> totalPiecesForGamePlay;
+    
     private Piece[] singles;
     private Colonel[] colonels;
     private Major[] majors;
@@ -26,20 +31,23 @@ public class Computer
     private Miner[] miners;
     private Scout[] scouts;
     private Bomb[] bombs;
-    private int count;
-
-
+    
+    
     public Computer()
     {
-        count = 0;
+        board = new Board();
+        totalPiecesForGamePlay = new ArrayList(40);
         instantiatePieceArrays();
         addSingles();
         addPieces();
+        addToTotalList();
         boardSetUp();
     }
-
+    
     private void instantiatePieceArrays()
     {
+        totalPiecesForSetup = new ArrayList(40);
+        
         singles = new Piece[4];
         colonels = new Colonel[2];
         majors = new Major[3];
@@ -48,11 +56,11 @@ public class Computer
         sergeants = new Sergeant[4];
         miners = new Miner[5];
         scouts = new Scout[8];
+        bombs = new Bomb[6];
     }
-
+    
     private void addPieces()
     {
-        addSingles();
         for (int i = 0; i < colonels.length; i++)
             colonels[i] = new Colonel();
         for (int i = 0; i < majors.length; i++)
@@ -70,7 +78,7 @@ public class Computer
         for (int i = 0; i < bombs.length; i++)
             bombs[i] = new Bomb();
     }
-
+    
     private void addSingles()
     {
         singles[0] = new Marshal();
@@ -78,74 +86,117 @@ public class Computer
         singles[2] = new Spy();
         singles[3] = new Flag();
     }
-
-
+    
+    
     public void boardSetUp()
     {
         flagSetUp();
         randomPieceSetup();
     }
-
+    
     public void flagSetUp()
     {
         int flagPos;            //1, 2 or 3 representing left corner, random in middle or right corner
         int bombsToProtect;     //The number of bombs allocated to protect the flag
-
-
+        int flagCol = -1;            //randomized column of the flag in the random middle scenario
+        
+        
         //Decide where flag should be
         flagPos = r.nextInt(3) + 1;
         switch (flagPos)
         {
-            case 1 : singles[3].setCell(0, 0);
+            case 1 : 
+                board.getCell(0, 0).put(singles[3]);        //set flag
+                board.getCell(1, 0).put(bombs[0]);          //set bomb 1
+                board.getCell(0, 1).put(bombs[1]);          //set bomb 2
                 break;
-            case 2 : singles[3].setCell(0, (r.nextInt(7) + 1));
+            case 2 : 
+                flagCol = r.nextInt(7) + 1;                 //random col
+                board.getCell(0, flagCol).put(singles[3]);  //set flag
                 break;
-            case 3 : singles[3].setCell(0, 9);
+            case 3 : 
+                board.getCell(0, 9).put(singles[3]);        //set flag
+                board.getCell(0, 8).put(bombs[0]);          //set bomb 1
+                board.getCell(1, 9).put(bombs[1]);          //set bomb 2
+                break;
         }
-
+        
         //decide how many bombs should be set around flag
         if (flagPos == 2)               //in the center
         {
-            bombsToProtect = r.nextInt(4) + 2;
+            bombsToProtect = r.nextInt(4) + 2;              //find number of bombs to use (between 2 and 5)
             switch (bombsToProtect)
             {
-                case 2 :
-                    bombs[0].setCell(1, singles[3].getCell().getCol() - 1);
-                    bombs[1].setCell(1, singles[3].getCell().getCol() + 1);
+                case 2 : //for 2 bombs
+                    board.getCell(1, (flagCol - 1)).put(bombs[0]);
+                    board.getCell(1, (flagCol + 1)).put(bombs[1]);
                     break;
-                case 3 :
-                    bombs[0].setCell(0, singles[3].getCell().getCol() - 1);
-                    bombs[1].setCell(0, singles[3].getCell().getCol() + 1);
-                    bombs[2].setCell(1, singles[3].getCell().getCol());
+                case 3 : //for 3 bombs
+                    board.getCell(0, (flagCol - 1)).put(bombs[0]);
+                    board.getCell(0, (flagCol + 1)).put(bombs[1]);
+                    board.getCell(1, flagCol).put(bombs[2]);
                     break;
-                case 4 :
-                    bombs[0].setCell(0, singles[3].getCell().getCol() - 1);
-                    bombs[1].setCell(1, singles[3].getCell().getCol() - 1);
-                    bombs[3].setCell(1, singles[3].getCell().getCol() + 1);
-                    bombs[4].setCell(0, singles[3].getCell().getCol() + 1);
+                case 4 :    //for four bombs
+                    board.getCell(0, (flagCol - 1)).put(bombs[0]);
+                    board.getCell(1, (flagCol - 1)).put(bombs[1]);
+                    board.getCell(1, (flagCol + 1)).put(bombs[2]);
+                    board.getCell(0, (flagCol + 1)).put(bombs[3]);
                     break;
-                case 5:
-                    bombs[0].setCell(0, singles[3].getCell().getCol() - 1);
-                    bombs[1].setCell(1, singles[3].getCell().getCol() - 1);
-                    bombs[2].setCell(1, singles[3].getCell().getCol());
-                    bombs[3].setCell(1, singles[3].getCell().getCol() + 1);
-                    bombs[4].setCell(0, singles[3].getCell().getCol() + 1);
+                case 5:     //for five bombs
+                    board.getCell(0, (flagCol - 1)).put(bombs[0]);
+                    board.getCell(1, (flagCol - 1)).put(bombs[1]);
+                    board.getCell(1, flagCol).put(bombs[2]);
+                    board.getCell(1, (flagCol + 1)).put(bombs[3]);
+                    board.getCell(0, (flagCol + 1)).put(bombs[4]);
                     break;
             }
         }
-        else if (flagPos == 1)          //in the left corner
-        {
-            bombs[0].setCell(1, 0);
-            bombs[1].setCell(0, 1);
-        }
-        else
-        {
-            bombs[0].setCell(0, 8);
-            bombs[1].setCell(1, 9);
-        }
-
     }
-
-    public void randomPieceSetup()
-    { }
+    
+    private void addToTotalList()
+    {
+        totalPiecesForSetup.addAll(Arrays.asList(singles));
+        totalPiecesForSetup.addAll(Arrays.asList(colonels));
+        totalPiecesForSetup.addAll(Arrays.asList(majors));
+        totalPiecesForSetup.addAll(Arrays.asList(captains));
+        totalPiecesForSetup.addAll(Arrays.asList(lieutenants));
+        totalPiecesForSetup.addAll(Arrays.asList(sergeants));
+        totalPiecesForSetup.addAll(Arrays.asList(miners));
+        totalPiecesForSetup.addAll(Arrays.asList(scouts));
+        totalPiecesForSetup.addAll(Arrays.asList(bombs));
+    }
+    
+    private boolean checkIfCellIsFree(int row, int col)
+    {
+        if (board.getCell(row, col) == null)
+            return false;
+        return true;
+    }
+    
+    private void randomPieceSetup()
+    {
+        
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Piece curPiece = totalPiecesForSetup.get(r.nextInt(totalPiecesForSetup.size()));
+                totalPiecesForSetup.remove(curPiece);
+                totalPiecesForGamePlay.add(curPiece);
+                if (checkIfCellIsFree(i, j))
+                    board.getCell(i, j).put(curPiece);
+                else
+                    j++;
+            }
+        }
+    }
+    
+    public void makeRandMove(ArrayList<Piece> availablePieces)
+    {
+        Piece chosen = availablePieces.get(r.nextInt(availablePieces.size()));
+        chosen
+        
+    }
+    
+    
 }
